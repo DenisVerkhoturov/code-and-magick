547ead29ca03b65f168862011a00e718f0f6a87d
Leo.Scream <mod.satyr@gmail.com>
2016-05-27 10:02:29 +0800

    Написал свою функцию проверки типа значения. getMessage теперь выкинет ошибку, если пользователь функции передал параметры с которыми она не работает (поведение в задаче не было оговорено), переписал лямбда выражения на анонимные функции (возвращение к ECMA 5.1), условие решения ситуации когда переданы два массива не полное, отсутствует логика работы с массивами разной длинны (на данный момент массив {a} берется как ведущий и для всех его элементов происходит сворачивание с вкраплением соответствующих эллементов из массива {b}. если массив {b} меньше, то каждое новое значение из него будет 0)

--- build/js/check.js
+++ build/js/check.js
@@ -1,28 +1,53 @@
 'use strict';
 
+/**
+ * Возвращает строку сообщения, сформированную в зависимости от типов переданных значений.
+ * @param a {boolean||Boolean||number||Number||Array}
+ * @param b {string||number||Number||Array}
+ * @returns {string}
+ * @throws {Error} Если переданны параметры, с которыми функция не работает.
+ */
 function getMessage(a, b)
 {
     var message;
 
-    if (typeof a == 'boolean') {
-        if (a) {
-            message = 'Я попал в ' + b
+    if (_assert(a, 'Boolean')) {
+        if (a.valueOf()) {
+            message = 'Я попал в ' + b;
         } else {
             message = 'Я никуда не попал';
         }
     }
-    else if (typeof a == 'number') {
+    else if (_assert(a, 'Number')) {
         message = 'Я прыгнул на ' + (a * 100) + ' сантиметров';
     }
-    else if (typeof a == 'object') {
-        if (typeof b == 'object' && b != null) {
-            let length = a.reduce((result, current, index) => result + (current * b[index]));
+    else if (_assert(a, 'Array')) {
+        if (_assert(b, 'Array')) {
+            var length = a.reduce(function(result, current, index) {
+                return result + current * (b[index] || 0);
+            }, 0);
             message = 'Я прошёл ' + length + ' метров';
         } else {
-            let sum = a.reduce((result, current) => result + current);
+            var sum = a.reduce(function(result, current) {
+                return result + current;
+            }, 0);
             message = 'Я прошёл ' + sum + ' шагов';
         }
+    } else {
+        throw new Error('Функция не работает с переданными параметрами: a: {' + a + '}, b: {' + b + '}');
     }
 
     return message;
+}
+
+/**
+ * Проверка соответствия типов данных.
+ * @param value - значение тип, которого необходимо проверить.
+ * @param expectedType - тип, c которым соотносим переданное значение.
+ * @returns {boolean}
+ * @private
+ */
+function _assert(value, expectedType)
+{
+    return Object.prototype.toString.call(value).slice(8, -1) == expectedType;
 }
