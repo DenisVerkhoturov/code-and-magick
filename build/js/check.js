072c36af0a0e0c9f8e2d118e27c5d744727b207f
Leo.Scream <mod.satyr@gmail.com>
2016-05-28 12:02:34 +0800

    Вернул код в простейшее состояние, прошелся по репозиторию ESLint'ом и подключил editorsconfig.

--- build/js/check.js
+++ build/js/check.js
@@ -7,59 +7,31 @@
  * @returns {string}
  */
 function getMessage(a, b) {
-    var message;
+  var message;
 
-    if (_.variable(a).ofType('Boolean')) {
-        if (a.valueOf()) {
-            message = 'Я попал в ' + b;
-        } else {
-            message = 'Я никуда не попал';
-        }
-    }
-    else if (_.variable(a).ofType('Number')) {
-        message = 'Я прыгнул на ' + (a * 100) + ' сантиметров';
-    }
-    else if (_.variable(a).ofType('Array')) {
-        if (_.variable(b).ofType('Array')) {
-            var length = a.reduce(function (result, current, index) {
-                return result + current * (b[index] || 0);
-            }, 0);
-            message = 'Я прошёл ' + length + ' метров';
-        } else {
-            var sum = a.reduce(function (result, current) {
-                return result + current;
-            }, 0);
-            message = 'Я прошёл ' + sum + ' шагов';
-        }
+  if (typeof a === 'boolean') {
+    if (a.valueOf()) {
+      message = 'Я попал в ' + b;
     } else {
-        message = '';
+      message = 'Я никуда не попал';
     }
+  } else if (typeof a === 'number') {
+    message = 'Я прыгнул на ' + (a * 100) + ' сантиметров';
+  } else if (Array.isArray(a)) {
+    if (Array.isArray(b)) {
+      var length = a.reduce(function (result, current, index) {
+        return result + current * (b[index] || 0);
+      }, 0);
+      message = 'Я прошёл ' + length + ' метров';
+    } else {
+      var sum = a.reduce(function (result, current) {
+        return result + current;
+      }, 0);
+      message = 'Я прошёл ' + sum + ' шагов';
+    }
+  } else {
+    message = '';
+  }
 
-    return message;
+  return message;
 }
-
-/**
- * Объект вспомогательных функций, будет расширяться по мере необходимости.
- */
-var _ = {
-    /**
-     * Установиливает значение в текущий контекст.
-     * @param variable
-     * @memberOf {_}
-     * @returns {_}
-     */
-    variable: function (variable) {
-        this.context = variable;
-        return this;
-    },
-
-    /**
-     * Проверяет, является ли текущее значение контекста ожидаемого типа.
-     * @param expectedType
-     * @memberOf {_}
-     * @returns {boolean}
-     */
-    ofType: function (expectedType) {
-        return Object.prototype.toString.call(this.context).slice(8, -1) == expectedType;
-    }
-};
