91e842aa83ddfdacab74f8482f33ac9963371bb1
Leo.Scream <mod.satyr@gmail.com>
2016-06-18 12:54:54 +0800

    Валидация формы

--- src/form.js
+++ src/form.js
@@ -1,17 +1,57 @@
 'use strict';
 
 (function() {
-  var formContainer = document.querySelector('.overlay-container');
-  var formOpenButton = document.querySelector('.reviews-controls-new');
-  var formCloseButton = document.querySelector('.review-form-close');
+  var formContainer = document.querySelector('.overlay-container'),
+    formOpenButton = document.querySelector('.reviews-controls-new'),
+    formCloseButton = document.querySelector('.review-form-close'),
+    form = document.querySelector('form.review-form'),
+    markGroup = form.querySelector('.review-form-group-mark'),
+    nameInput = form.querySelector('.review-form-field-name'),
+    textInput = form.querySelector('.review-form-field-text'),
+    reviewFields = form.querySelector('.review-fields'),
+    reviewFieldsName = reviewFields.querySelector('[for="review-name"]'),
+    reviewFieldsText = reviewFields.querySelector('[for="review-text"]'),
+    formSubmitButton = form.querySelector('button[type="submit"]'),
+    getCurrentMark = function() {
+      return markGroup.querySelector('input[name="review-mark"]:checked').value;
+    },
+    formValidate = function() {
+      textInput.required = getCurrentMark() < 3;
 
-  formOpenButton.onclick = function(evt) {
+      if (nameInput.checkValidity()) {
+        reviewFieldsName.classList.add('invisible');
+      } else {
+        reviewFieldsName.classList.remove('invisible');
+      }
+
+      if (textInput.checkValidity()) {
+        reviewFieldsText.classList.add('invisible');
+      } else {
+        reviewFieldsText.classList.remove('invisible');
+      }
+
+      if (nameInput.checkValidity() && textInput.checkValidity()) {
+        reviewFields.classList.add('invisible');
+        formSubmitButton.disabled = false;
+      } else {
+        reviewFields.classList.remove('invisible');
+        formSubmitButton.disabled = true;
+      }
+    };
+
+  formOpenButton.addEventListener('click', function(evt) {
     evt.preventDefault();
     formContainer.classList.remove('invisible');
-  };
+  });
 
-  formCloseButton.onclick = function(evt) {
+  formCloseButton.addEventListener('click', function(evt) {
     evt.preventDefault();
     formContainer.classList.add('invisible');
-  };
+  });
+
+  markGroup.addEventListener('change', formValidate);
+  nameInput.addEventListener('input', formValidate);
+  textInput.addEventListener('input', formValidate);
+  nameInput.required = true;
+  formValidate();
 })();
