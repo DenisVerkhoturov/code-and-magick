51e5d551185d1667f9a2a2d3ef991896dbc1019e
Leo.Scream <mod.satyr@gmail.com>
2016-07-02 17:01:02 +0800

    Переписал галерею на объект

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ require(
     './utilities',
     './form',
     './game',
-    './reviews',
-    './gallery'
+    './gallery',
+    './reviews'
   ],
-  function(utils, form, Game, reviews, gallery) {
+  function(utils, form, Game, gallery, reviews) {
     var game = new Game(document.querySelector('.demo')),
       clouds = document.querySelector('.header-clouds'),
       isCloudsVisible = true;
@@ -27,19 +27,6 @@ require(
 
       if (game.container.getBoundingClientRect().bottom < 0) {
         game.setGameStatus(game.Verdict.PAUSE);
-      }
-    });
-
-    var photoGallery = document.querySelector('.photogallery'),
-      pictures = [].map.call(photoGallery.querySelectorAll('.photogallery-image img'), function(img) {
-        return img.src;
-      });
-
-    gallery.setPictures(pictures);
-
-    photoGallery.addEventListener('click', function(evt) {
-      if (evt.target.nodeName === 'IMG') {
-        gallery.show(pictures.indexOf(evt.target.src));
       }
     });
 
